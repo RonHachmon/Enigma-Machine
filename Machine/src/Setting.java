cdb81a6f9f863a9110c1b2e229ca96243912f181
sagi goldfarb <84674218+sagiGold@users.noreply.github.com>
2022-08-16 04:01:27 +0300

    Working on main menu consider adding DTO's for UI requests + Exceptions class

--- Machine/src/Setting.java
+++ Machine/src/Setting.java
@@ -1,97 +1,76 @@
-import java.util.Collections;
-
 public class Setting {
 
-    // sperated by index
+    // seperated by index
     // 0 - rotors
     // 1 - starting index for rotor
     // 2 - reflector
     // 3 plug
     //EXAMPLE :    0      1    2      3
     //        <45,27,94><AO!><III><A|Z,D|E>
-    private String [] currentCodeSettings = new String [4] ;
+    private String[] currentCodeSettings = new String[4];
 
-
-    public String getFullMachineCode()
-    {
-        String code="";
-        for(String string:currentCodeSettings)
-        {
-            if(string!=null) {
+    public String getFullMachineCode() {
+        String code = "";
+        for (String string : currentCodeSettings) {
+            if (string != null) {
                 if (string.charAt(string.length() - 1) == ',') {
                     string = string.substring(0, string.length() - 1);
                 }
                 code += '<' + string + '>';
             }
-
         }
         return code;
     }
 
-
-    public void setSettingRotators(Integer [] selectedRotators)
-    {
-        int i=0;
+    public void setSettingRotators(Integer[] selectedRotators) {
+        int i = 0;
         //reset
-        currentCodeSettings[0]="";
-        for ( i = 0; i <selectedRotators.length-1 ; i++) {
+        currentCodeSettings[0] = "";
+        for (i = 0; i < selectedRotators.length - 1; i++) {
 
-            currentCodeSettings[0]+=selectedRotators[i].toString();
-            currentCodeSettings[0]+=',';
-
+            currentCodeSettings[0] += selectedRotators[i].toString();
+            currentCodeSettings[0] += ',';
         }
         //last one without ','
-        currentCodeSettings[0]+=selectedRotators[i].toString();
+        currentCodeSettings[0] += selectedRotators[i].toString();
     }
-    public void addSettingRotators(Integer oneRotor)
-    {
+
+    public void addSettingRotators(Integer oneRotor) {
         initializeStringIfNull(0);
-
-        currentCodeSettings[0]+=oneRotor.toString()+',';
-
+        currentCodeSettings[0] += oneRotor.toString() + ',';
     }
+
     //gets full set of characters for example "AO!"
-    public void setSettingStartingChars(String startingCharArray)
-    {
+    public void setSettingStartingChars(String startingCharArray) {
         initializeStringIfNull(1);
         StringBuilder array = new StringBuilder(startingCharArray);
         array.reverse();
-        currentCodeSettings[1]=array.toString();
+        currentCodeSettings[1] = array.toString();
     }
 
     //gets chars one by one
-    public void addSettingStartingChars(String oneCharacter)
-    {
+    public void addSettingStartingChars(String oneCharacter) {
         initializeStringIfNull(1);
-
-        currentCodeSettings[1]+=oneCharacter;
-
+        currentCodeSettings[1] += oneCharacter;
     }
-    public void setSettingReflector(int selectedReflector)
-    {
 
-        currentCodeSettings[2]=Setting.converteIntToRoman(selectedReflector);
-
+    public void setSettingReflector(int selectedReflector) {
+        currentCodeSettings[2] = Setting.convertIntToRoman(selectedReflector);
     }
-    public void addPlug(Character firstChar,Character secondChar)
-    {
+
+    public void addPlug(Character firstChar, Character secondChar) {
         initializeStringIfNull(3);
-
-        currentCodeSettings[3]+=firstChar.toString()+'|'+secondChar.toString()+',';
-
+        currentCodeSettings[3] += firstChar.toString() + '|' + secondChar.toString() + ',';
     }
 
     private void initializeStringIfNull(int index) {
-        if(currentCodeSettings[index]==null)
-        {
-            currentCodeSettings[index]="";
+        if (currentCodeSettings[index] == null) {
+            currentCodeSettings[index] = "";
         }
     }
 
-    private static String converteIntToRoman(int number)
-    {
-        switch (number)
-        {
+    private static String convertIntToRoman(int number) {
+        switch (number) {
             case 1:
                 return "I";
             case 2:
@@ -104,8 +83,6 @@ public class Setting {
                 return "V";
             default:
                 throw new IllegalArgumentException("Invalid Reflector,id must between 1-5 ");
-
         }
     }
-
 }
---
+++ Machine/src/Statistic.java
@@ -0,0 +1,7 @@
+public class Statistic {
+
+    //need to be implemented
+    public String getAmountOfProccessedInputs() {
+        return null;
+    }
+}
